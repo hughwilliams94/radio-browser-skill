a45dbe2b8c4c2735b1485ccac08e5558ef840dbc
Hugh Williams <hugh+github@hmlw.uk>
2020-07-10 14:08:49 +0100

    Adding some docstrings and formatting changes

--- __init__.py
+++ __init__.py
@@ -12,29 +12,39 @@ from mycroft.util.log import LOG
 
 
 def match_station_name(phrase):
+    """Takes the user utterance and attempts to match a specific station
+
+    :param phrase: User utterance
+    :type phrase: str
+
+    :return: A tuple containing the original phrase, the CPS.MatchLevel,
+    and a dictionary containing the resolved stream URL.
+    :rtype: tuple
+    """
+    numbers_regex = r"\b(one|two|three|four|five|six|seven|eight|nine)\b"
     try:
         rb = RadioBrowser()
     except Exception as e:
         LOG.exception("Failed to load pyradios" + repr(e))
     LOG.info(f"Searching for {phrase}")
     results = rb.search(name=phrase)
-    parsed_results = json.loads(json.dumps(results))
+    parsed = json.loads(json.dumps(results))
 
-    if len(parsed_results) > 0:
-        LOG.info(
-            f"Found {parsed_results[0]['name']} URL: {parsed_results[0]['url_resolved']}"
-        )
-        return phrase, CPSMatchLevel.EXACT, {"url": parsed_results[0]["url_resolved"]}
+    if len(parsed) > 0:
+        # If an exatch match has been found return.
+        LOG.info(f"Found {parsed[0]['name']} ({parsed[0]['url_resolved']})")
+        return phrase, CPSMatchLevel.EXACT, {"url": parsed[0]["url_resolved"]}
     elif re.search(" [0-9]+ ", phrase):
-        # Replace any digits (1,2,3) with text (one,two,three) and repeat search.
+        # If no match has been found, replace any digits (1,2,3) with text
+        # (one, two, three) and repeat search.
         num = re.findall("[0-9]+", phrase)
         inf_eng = inflect.engine()
         for number in num:
             phrase = phrase.replace(num, inf_eng.number_to_words(number))
         match_station_name(phrase)
-    elif re.search(r"\b(one|two|three|four|five|six|seven|eight|nine)\b", phrase):
+    elif re.search(numbers_regex, phrase):
         # As above but reversed: change strings to ints and repeat search.
-        num = re.findall(r"\b(one|two|three|four|five|six| seven|eight|nine)\b", phrase)
+        num = re.findall(numbers_regex, phrase)
         for number in num:
             phrase = phrase.replace(number, str(w2n.word_to_num(number)))
         match_station_name(phrase)
@@ -43,7 +53,17 @@ def match_station_name(phrase):
 
 
 def match_genre(phrase):
-    # Strip 'a' and 'station' from phrases like 'Play a jazz station.' to get genre.
+    """Takes the user utterance and attempts to match a genre of station,
+    returning the most popular of that genre.
+
+    :param phrase: User utterance
+    :type phrase: str
+
+    :return: A tuple containing the original phrase, the CPS.MatchLevel,
+    and a dictionary containing the resolved stream URL.
+    :rtype: tuple
+    """
+    # Strip 'a' and 'station' from phrases like 'Play a jazz station.'.
     stripped_phrase = phrase.lower().replace("a ", "").replace(" station", "")
     try:
         rb = RadioBrowser()
@@ -51,13 +71,11 @@ def match_genre(phrase):
         LOG.exception("Failed to load pyradios" + repr(e))
     LOG.info(f"Searching for a {stripped_phrase} station")
     results = rb.search(tag=stripped_phrase, order="votes")
-    parsed_results = json.loads(json.dumps(results))
+    parsed = json.loads(json.dumps(results))
 
-    if len(parsed_results) > 0:
-        LOG.info(
-            f"Found {parsed_results[0]['name']} URL: {parsed_results[0]['url_resolved']}"
-        )
-        return phrase, CPSMatchLevel.EXACT, {"url": parsed_results[0]["url_resolved"]}
+    if len(parsed) > 0:
+        LOG.info(f"Found {parsed[0]['name']} ({parsed[0]['url_resolved']})")
+        return phrase, CPSMatchLevel.EXACT, {"url": parsed[0]["url_resolved"]}
     else:
         match_station_name(phrase)
 
