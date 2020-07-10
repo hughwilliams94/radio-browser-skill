75a18f778869c35f7e9f08bebf1d6f670f894b14
Hugh Williams <hugh+github@hmlw.uk>
2020-07-10 10:45:39 +0100

    Station matching function

--- __init__.py
+++ __init__.py
@@ -1,6 +1,44 @@
 #!/usr/bin/env python3
 
+import re
+import json
+import inflect
+
+from pyradios import RadioBrowser
+from word2number import w2n
+
 from mycroft.skills.common_play_skill import CommonPlaySkill, CPSMatchLevel
+from mycroft.util.log import LOG
+
+
+def match_station_name(phrase):
+    try:
+        rb = RadioBrowser()
+    except Exception as e:
+        LOG.exception('Failed to load pyradios' + repr(e))
+    LOG.info(f"Searching for {phrase}")
+    results = rb.search(name=phrase)
+    parsed_results = json.loads(json.dumps(results))
+
+    if len(parsed_results) > 0:
+        LOG.info(f"Found {parsed_results[0]['name']} URL: {parsed_results[0]['url_resolved']}")
+        return phrase, CPSMatchLevel.EXACT, {"url": parsed_results[0]['url_resolved']}
+    elif re.search(' [0-9]+ ', phrase):
+        # Replace any digits (1,2,3) with text (one,two,three) and repeat search.
+        num = re.findall('[0-9]+', phrase)
+        inf_eng = inflect.engine()
+        for number in num:
+            phrase = phrase.replace(num, inf_eng.number_to_words(number))
+        match_station_name(phrase)
+    elif re.search(r'\b(one|two|three|four|five|six| seven|eight|nine)\b', phrase):
+        # As above but reversed: change strings to ints and repeat search.
+        num = re.findall(r'\b(one|two|three|four|five|six| seven|eight|nine)\b', phrase)
+        for number in num:
+            phrase = phrase.replace(number, str(w2n.word_to_num(number)))
+        match_station_name(phrase)
+    else:
+        return None
+
 
 class RadioBrowserSkill(CommonPlaySkill):
     def __init__(self):
